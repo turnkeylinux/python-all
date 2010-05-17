8bd6d7a834dddfd32f8d6be7b202822661414404
Matthias Klose <doko@timbuktu>
2010-05-17 12:57:24 +0200

    package version python-defaults-2.4.3-02

--- debian/pyversions.py
+++ debian/pyversions.py
@@ -50,9 +50,9 @@ def supported_versions(version_only=False):
     global _supported_versions
     if not _supported_versions:
         if os.path.exists('/usr/share/python/debian_defaults'):
-            from ConfigParser import SafeConfigParser
-            config = SafeConfigParser()
-            config.readfp(file('/usr/share/python/debian_defaults'))
+            import ConfigParser
+            config = ConfigParser.SafeConfigParser()
+            config.read('/usr/share/python/debian_defaults')
             value = config.get('DEFAULT', 'supported-versions')
             _supported_versions = [s.strip() for s in value.split(',')]
         else:
@@ -92,10 +92,11 @@ def requested_versions(vstring, version_only=False):
     vinfo = parse_versions(vstring)
     supported = supported_versions(version_only=True)
     if len(vinfo) == 1:
+        vkey = vinfo[vinfo.keys()[0]]
         if 'all' in vinfo:
             versions = supported
         elif 'current' in vinfo:
-            versions = [default_version(version_only=True)]
+            versions = [default_version()]
         else:
             versions = vinfo['versions'].intersection(supported)
     elif 'all' in vinfo and 'current' in vinfo:
@@ -118,20 +119,13 @@ def requested_versions(vstring, version_only=False):
 
 def installed_versions(version_only=False):
     import glob
-    supported = supported_versions()
     versions = [os.path.basename(s)
-                for s in glob.glob('/usr/bin/python[0-9].[0-9]')
-                if os.path.basename(s) in supported]
+                for s in glob.glob('/usr/bin/python[0-9].[0-9]')]
     versions.sort()
     if version_only:
         return [v[6:] for v in versions]
     else:
         return versions
-
-class ControlFileValueError(ValueError):
-    pass
-class MissingVersionValueError(ValueError):
-    pass
 
 def extract_pyversion_attribute(fn, pkg):
     """read the debian/control file, extract the XS-Python-Version
@@ -143,13 +137,11 @@ def extract_pyversion_attribute(fn, pkg):
     for line in file(fn):
         line = line.strip()
         if line == '':
-            if pkg == 'Source':
-                break
             section = None
         elif line.startswith('Source:'):
             section = 'Source'
         elif line.startswith('Package: ' + pkg):
-            section = pkg
+            section = self.name
         elif line.startswith('XS-Python-Version:'):
             if section != 'Source':
                 raise ValueError, \
@@ -158,55 +150,15 @@ def extract_pyversion_attribute(fn, pkg):
         elif line.startswith('XB-Python-Version:'):
             if section == pkg:
                 version = line.split(':', 1)[1].strip()
-    if section == None:
-        raise ControlFileValueError, 'not a control file'
     if pkg == 'Source':
         if sversion == None:
-            raise MissingVersionValueError, \
-                  'missing XS-Python-Version in control file'
+            raise ValueError, 'missing XS-Python-Version in control file'
         return sversion
     if version == None:
-        raise MissingVersionValueError, \
+        raise ValueError, \
               'missing XB-Python-Version for package `%s' % pkg
     return version
 
-# compatibility functions to parse debian/pyversions
-
-def version_cmp(ver1,ver2):
-    v1=[int(i) for i in ver1.split('.')]
-    v2=[int(i) for i in ver2.split('.')]
-    return cmp(v1,v2)
-
-def requested_versions_bis(vstring, version_only=False):
-    versions = []
-    py_supported_short = supported_versions(version_only=True)
-    for item in vstring.split(','):
-        v=item.split('-')
-        if len(v)>1:
-            if not v[0]:
-                v[0] = py_supported_short[0]
-            if not v[1]:
-                v[1] = py_supported_short[-1]
-            for ver in py_supported_short:
-                try:
-                    if version_cmp(ver,v[0]) >= 0 \
-                           and version_cmp(ver,v[1]) <= 0:
-                        versions.append(ver)
-                except ValueError:
-                    pass
-        else:
-            if v[0] in py_supported_short:
-                versions.append(v[0])
-    versions.sort(version_cmp)
-    if not versions:
-        raise ValueError, 'empty set of versions'
-    if not version_only:
-        versions=['python'+i for i in versions]
-    return versions
-
-def extract_pyversion_attribute_bis(fn):
-    vstring = file(fn).readline().rstrip('\n')
-    return vstring
 
 def main():
     from optparse import OptionParser
@@ -220,7 +172,7 @@ def main():
                       action='store_true', dest='supported')
     parser.add_option('-r', '--requested',
                       help='print the python versions requested by a build; the argument is either the name of a control file or the value of the XS-Python-Version attribute',
-                      action='store_true', dest='requested')
+                      action='store', dest='versions')
     parser.add_option('-i', '--installed',
                       help='print the installed supported python versions',
                       action='store_true', dest='installed')
@@ -230,46 +182,24 @@ def main():
     opts, args = parser.parse_args()
     program = os.path.basename(sys.argv[0])
 
-    if opts.default and len(args) == 0:
+    if opts.default:
         print default_version(opts.version_only)
-    elif opts.supported and len(args) == 0:
+    elif opts.supported:
         print ' '.join(supported_versions(opts.version_only))
-    elif opts.installed and len(args) == 0:
+    elif opts.installed:
         print ' '.join(installed_versions(opts.version_only))
-    elif opts.requested and len(args) <= 1:
-        if len(args) == 0:
-            versions = 'debian/control'
-        else:
-            versions = args[0]
+    elif opts.versions:
         try:
-            if os.path.isfile(versions):
-                fn = versions
-                try:
-                    vstring = extract_pyversion_attribute(fn, 'Source')
-                    vs = requested_versions(vstring, opts.version_only)
-                except ControlFileValueError:
-                    sys.stderr.write("%s: not a control file: %s, " \
-                                     % (program, fn))
-                    sys.exit(1)
-                except MissingVersionValueError:
-                    fn = os.path.join(os.path.dirname(fn), 'pyversions')
-                    sys.stderr.write("%s: missing XS-Python-Version in control file, fall back to %s\n" \
-                                     % (program, fn))
-                    try:
-                        vstring = extract_pyversion_attribute_bis(fn)
-                        vs = requested_versions_bis(vstring, opts.version_only)
-                    except IOError:
-                        sys.stderr.write("%s: missing debian/pyversions file, fall back to supported versions\n" \
-                                         % program)
-                        vs = supported_versions(opts.version_only)
+            if os.path.isfile(opts.versions):
+                vs = extract_pyversion_attribute(opts.versions, 'Source')
             else:
-                vs = requested_versions(versions, opts.version_only)
-            print ' '.join(vs)
+                vs = opts.versions
+            print ' '.join(requested_versions(vs, opts.version_only))
         except ValueError, msg:
-            sys.stderr.write("%s: %s\n" % (program, msg))
+            print "%s: %s" % (program, msg)
             sys.exit(1)
     else:
-        sys.stderr.write("usage: %s %s\n" % (program, usage))
+        print "usage: %s %s" % (program, usage)
         sys.exit(1)
 
 if __name__ == '__main__':
