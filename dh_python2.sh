1e8004f53f60d9128a67c649d3c49006c7350281
Piotr Ożarowski <piotr@debian.org>
2013-09-12 22:46:25 +0200

    Replace /usr/bin/dh_python2 with a Python script that invokes dh-python's dh_python2 if Build-Depends{,-Indep} contains dh-python

--- dh_python2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ -f /usr/share/dh-python/dh_python2 ] &&\
-    grep -q dh-python ./debian/control 2>/dev/null
-then
-  exec /usr/share/dh-python/dh_python2 $@
-else
-  exec /usr/share/python/dh_python2 $@
-fi
